785dd6622d63e0c5a91c438aea4bbcd91fbea7e1
Giovanni De Toni <giovanni.det@gmail.com>
2017-03-07 12:47:29 +0100

    Add a more informative message and updated years.

--- pagecounts/data/make_dirs.sh
+++ pagecounts/data/make_dirs.sh
@@ -5,12 +5,12 @@ IFS=$'\n\t'
 year=''
 month=''
 
-for year in {2007..2015}; do
+for year in {2007..2016}; do
     for month in {01..12}; do
 
         if [ "$year" -eq "2007" -a "$month" -lt "12" ]; then continue; fi
 
-        echo "${year}-${month}"
-        mkdir -p "${year}-${month}" 
+        echo "[*] Making dir ${year}-${month}"
+        mkdir -p "${year}-${month}"
     done
 done
