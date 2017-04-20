7ef0db81ab71d79519ebd37aa6650d0544f015df
Giovanni De Toni <giovanni.det@gmail.com>
2017-04-20 16:22:26 +0200

    [generate_weekly_data.py] Add code documentation and author information.

--- data_analysis/generate_weekly_data.py
+++ data_analysis/generate_weekly_data.py
@@ -1,36 +1,38 @@
+#!/usr/bin/env python
+# Given a complete year files with data in the form (page, week, visits)
+# this script will generate a convenient csv file which will store for
+# each page and for each years's week the total number of visits.
+#
+# Written by Giovanni De Toni (2017)
+# Email: giovanni.det at gmail.com
+
 import fileinput
 import pandas as pd
 
-# Set up a dictionary
+# Set up an empty dictionary
 all_data={}
-
-number=0
 
 # Read from standard input
 for line in fileinput.input():
 
     # Split the line given
+    # 0: page name
+    # 1: week number
+    # 2: visits on that week
     result = str.split(line)
-	
-    # Print the line number analyzed
-    print(number)
 
     # Set up an empty list if the key
     # is null
     if all_data.get(result[0], []) == []:
         all_data[result[0]] = [0 for x in range(53)]
 
-    # Sum the visit counter
+    # Sum the visits
     all_data[result[0]][int(result[1])] += int(result[2]);
-    number += 1;
-	
-# Print all the data
-#for key, value in all_data.iteritems():
-#    print(key)
-#    print(value)
 
 # Generate a pandas dataframe with all the data
 df = pd.DataFrame(all_data);
+
+# Print the dataframe to show the result
 print(df)
 
 # Save it to file
