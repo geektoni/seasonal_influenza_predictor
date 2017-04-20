141aee95d4a483624abfaf4c143f2c78a70392cc
Giovanni De Toni <giovanni.det@gmail.com>
2017-04-20 10:46:32 +0200

    Change getopts with docopt.
    Change also the script usage pattern. It takes only an input/output
    directory where it will search for *.gz files. There is also a --debug
    flag to display more informations.

--- data_analysis/parse_dumps.sh
+++ data_analysis/parse_dumps.sh
@@ -4,79 +4,39 @@
 #
 # Written by Giovanni De Toni (2017)
 
-# Set up strict bash
-# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-IFS=$'\n\t'
+# General variables
+export input=''
+export output=''
+export debug=false
 
 # Usage and version information
-usage="
-Usage: parse_dumps [-o <file>] [-i <file>] [--debug]
-Options:
-	-o <output>  		Specify output directory
-	-i <input>   		Specify input directory
-	-y <year>			Year that should be analyzed
-	-d	 				Run with debug option.
-	-h					Show help options.
-	-v		  			Print program version.
-"
+eval "$(docopts -V - -h - : "$@" <<EOF
+Usage: parse_dumps [-i <input>] [-o <output>] [--debug]
 
-version=<<EOF
+Options:
+	-i <input>		Specify input directory (default is ./input)
+	-o <output>		Specify output directory (default is ./output)
+	--debug			Run with debug option.
+	--help			Show help options.
+	--version		Print program version.
+----
 parse_dumps 0.1.0
 Copyright (C) 2017 Giovanni De Toni
 License MIT
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 EOF
+)"
 
-# General variables
-export debug=0
-export new_input=0
-export input="/mnt/fluiddata/cconsonni/pagecounts/data/output"
-export output="./output"
-export year="2007"
-
-# Parse command line arguments
-while getopts ":i:o:y:dvh" opt; do
-	case $opt in
-		i)
-			export input=$OPTARG
-			export new_input=1
-      		;;
-		o)
-			export output=$OPTARG
-			;;
-		y)
-			export year=$OPTARG
-			;;
-		d)
-			export debug=1
-			;;
-		v)
-			#FIXME
-			echo $version
-			exit 0
-			;;
-		h)
-			#FIXME
-			echo $usage
-			exit 0
-			;;
-    	\?)
-      		echo "Invalid option: -$OPTARG" >&2
-      		exit 1
-      		;;
-    	:)
-      		echo "Option -$OPTARG requires an argument." >&2
-      		exit 1
-      		;;
-  esac
-done
+# Set up strict bash
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
 
 # Set up utility variables
 # and debug function
 function print_debug {
-	if [ $debug -ne 0 ]; then echo ${1:-}; fi
+	if $debug; then echo ${1:-}; fi
 }
 
 # Check all files for pattern
@@ -100,11 +60,13 @@ function parse_files {
 
 }
 
+# Add default values if -i or -o were not set
+if [ -z $input ]; then input='./input'; fi
+if [ -z $output ]; then output='./output'; fi
+
 print_debug "[*] Debug value --> $debug"
 print_debug "[*] Input directory value --> $input"
 print_debug "[*] Output directory value --> $output"
-print_debug "[*] Year requested --> $year"
-print_debug "[*] New input --> $new_input"
 
 # Set keywords, project and an other
 # section to add other constraints.
@@ -134,19 +96,6 @@ if [ ! -d $output ]; then
 	mkdir $output
 fi
 
-# Create year directory
-if [ ! -d $output/$year ]; then
-	mkdir $output/$year
-fi
-
-export output=$output/$year
-
-# Check if there is already a file
-if [ -f $output/$year.output ]; then
-	echo "The file $year.output already exists."
-	exit 1
-fi
-
 # Generate the regexp concatenating
 # all the keyword
 export regexp="$project ("
@@ -172,17 +121,9 @@ p_v=`parallel --version | grep parallel | head -1 | awk '{print $3}'`
 # Ugly way to manage custom input directories
 # FIXME
 if [ $p_v -ge 20141022 ]; then
-	if [ ! $new_input -eq 1 ]; then
-		ls $input/$year-*/output-*/*.gz | parallel --no-notice parse_files
-	else
-		ls $input/*.gz | parallel --no-notice parse_files
-	fi
+	ls $input/*.gz | parallel --no-notice parse_files
 else
-	if [ ! $new_input -eq 1 ]; then
-		ls $input/$year-*/output-*/*.gz | parallel parse_files
-	else
-		ls $input/*.gz | parallel parse_files
-	fi
+	ls $input/*.gz | parallel parse_files
 fi
 
 # Finally, merge together all the files
@@ -190,6 +131,6 @@ for f in $output/*.gz.output
 do
 	[[ -e $f ]] || break  # handle the case of no files
 	print_debug "[*] Merging file $f"
-	cat $f >> $output/$year.output
+	cat $f >> $output/result.output
 	rm -rf $f
 done
