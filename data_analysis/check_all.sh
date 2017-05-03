982e7f258412ae7f6001b459b9724814348de76f
Giovanni De Toni <giovanni.det@gmail.com>
2017-05-03 09:49:55 +0200

    Fix wrong file extension.

--- data_analysis/check_all.sh
+++ data_analysis/check_all.sh
@@ -7,9 +7,9 @@ IFS=$'\n\t'
 
 input_dir="/mnt/fluiddata/cconsonni/pagecounts/data/output"
 
-for year in {2008..2016}
+for year in {2009..2016}
 do
     all_files="$input_dir/$year-*/output-*"
     ./parse_dumps.sh -k "./../data/keywords.txt" -i "$all_files" -o "./output/$year" --debug
-    ./generate_weekly_data.py ./output/$year/result.csv ./output/$year/$year.csv
+    ./generate_weekly_data.py ./output/$year/result.output ./output/$year/$year.csv
 done
