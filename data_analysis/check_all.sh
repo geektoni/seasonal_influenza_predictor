bf4e0b13e60c3d0ef392836e1ae147f97bcfb746
Giovanni De Toni <giovanni.det@gmail.com>
2017-07-12 09:35:11 +0200

    [check_all] Update the script.

--- data_analysis/check_all.sh
+++ data_analysis/check_all.sh
@@ -5,11 +5,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-input_dir="/mnt/fluiddata/cconsonni/pagecounts/data/output"
+#input_dir="/mnt/fluiddata/cconsonni/pagecounts/data/output"
+input_dir="/mnt/fluiddata/cconsonni/pagecounts-new/pagecounts/data/output"
 
 for year in {2009..2016}
 do
     all_files="$input_dir/$year-*/output-*"
-    ./parse_dumps.sh -k "./../data/keywords.txt" -i "$all_files" -o "./output/$year" --debug
-    ./generate_weekly_data.py ./output/$year/result.output ./output/$year/$year.csv
+    ./parse_dumps.sh -k "./../data/keywords3.txt" -i "$all_files" -o "./output3/$year" --debug
+    ./generate_weekly_data.py ./output3/$year/result.output ./output3/$year/$year.csv -c ../data/keywords3.txt
 done
