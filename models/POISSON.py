14e1152f347a3162dd92e9c7c09ca447eb82fdf9
Giovanni De Toni <giovanni.det@gmail.com>
2017-07-30 21:55:05 +0200

    [models] Added a new Poisson model and upgraded linear one.

--- models/POISSON.py
+++ models/POISSON.py
@@ -10,17 +10,23 @@ Options:
 """
 
 import pandas as pd
-import numpy as np
+import numpy
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LassoCV
 from sklearn.metrics import mean_squared_error
 from tabulate import tabulate
-from colour import Color
 from docopt import docopt
+from colour import Color
 import csv
 
+from models_utils import generate, generate_labels, generate_labels_one_year, generate_one_year, cross_validation_glm
 
-from models_utils import generate, generate_labels, generate_labels_one_year, generate_one_year
+import glmnet_python
+from glmnet import glmnet; from glmnetPlot import glmnetPlot
+from glmnetPrint import glmnetPrint; from glmnetCoef import glmnetCoef; from glmnetPredict import glmnetPredict
+from cvglmnet import cvglmnet; from cvglmnetCoef import cvglmnetCoef
+from cvglmnetPlot import cvglmnetPlot; from cvglmnetPredict import cvglmnetPredict
+
+from pyglmnet import GLM
 
 #### INITIALIZATION #####
 
@@ -29,6 +35,10 @@ arguments = docopt(__doc__)
 
 # Year selected
 year_selected = int(arguments["<year>"])
+
+# Feature path and labels
+path_features = "./../data/wikipedia"
+path_labels = "./../data/influnet/csv"
 
 # Selected columns that will be extracted from the dataframes
 selected_columns = []
@@ -46,9 +56,10 @@ data = generate_one_year(year_selected)[selected_columns]
 labels_test = generate_labels_one_year(year_selected)["Incidenza Totale"]
 weeks = generate_labels_one_year(year_selected)["Settimana"]
 
-print("Train set shape (rows/columns): ", dataset.shape)
-print("Test set shape (rows/columns): ", data.shape)
-print("------------")
+print ("Train set shape (rows/columns): ", dataset.shape)
+print ("Test set shape (rows/columns): ", data.shape)
+print ("------------")
+
 
 # Create a copy of the dataset where we fills NaN values
 # with 0.
@@ -56,32 +67,34 @@ dataset_zero = dataset.fillna(0);
 data_zero = data.fillna(0);
 
 # Standardize data
-data_total = np.concatenate((dataset_zero, data_zero), axis=0)
+data_total = numpy.concatenate((dataset_zero, data_zero), axis=0)
 dmean = data_total.mean(axis=0)
 dmax = data_total.max(axis=0)
 dmin = data_total.min(axis=0)
 dmax_min = dmax-dmin
 dataset_imp = (dataset_zero-dmean)/dmax_min
 data_imp = (data_zero-dmean)/dmax_min
-dataset_imp[np.isnan(dataset_imp)] = 0
-data_imp[np.isnan(data_imp)] = 0
-
-# Regenerate panda dataframe with standardized data
-data_graph = pd.DataFrame(data=data_imp, columns=selected_columns);
+dataset_imp[numpy.isnan(dataset_imp)] = 0
+data_imp[numpy.isnan(data_imp)] = 0
 
 # Create a Lasso Cross-Validation instance which will be
 # trained on the dataset in which NaN values are replaced
-# with the column mean.
-lassoCV_imp = LassoCV(max_iter=100000, n_jobs=-1)
-lassoCV_imp.fit(dataset_imp, labels)
-result_lcv_imp = lassoCV_imp.predict(data_imp)
-print("LassoCV Standardized alpha: ", lassoCV_imp.alpha_)
+# with 0.
+#fit = cvglmnet(x = dataset_imp.copy(), y = labels.copy().as_matrix(), family = 'poisson', alpha=0.01, ptype="mse")
+#cvglmnetPlot(fit)
+result = cross_validation_glm(dataset_imp.as_matrix(), labels, data_imp, labels_test, runs=50)
+fit = result[0][1]
+result_lcv = cvglmnetPredict(fit, data_imp.as_matrix(), ptype = 'response', s = "lambda_min")
+coeff = cvglmnetCoef(fit, s = "lambda_min")
+
+# Regenerate panda dataframe with standardized data
+data_graph = pd.DataFrame(data=data_imp, columns=selected_columns)
 
 # Extract which features seems to be important
 # from the LASSO model
 important_pages=[]
 graph_pages=[]
-for i in list(zip(lassoCV_imp.coef_, selected_columns)):
+for i in list(zip(coeff, selected_columns)):
     if (i[0] != 0):
         if (i[0] > 0):
             graph_pages.append([i[1], i[0]])
@@ -94,28 +107,27 @@ important_pages = sorted(important_pages, key=getKey, reverse=True)
 graph_pages = sorted(graph_pages, key=getKey, reverse=True)
 
 # Print important pages
-print("------------")
-print("Pages which their weight is != from 0: ")
-print(tabulate(important_pages, headers=["Page", "Mean"]))
+print ("------------")
+print ("Pages which their weight is != from 0: ")
+print (tabulate(important_pages, headers=["Page", "Mean"]))
 
 # Prim MSE of the two models
-print("------------")
-#print("LASSO MSE: ", mean_squared_error(labels_test, result_lcv))
-print("LASSO Standardized MSE: ", mean_squared_error(labels_test, result_lcv_imp))
+print ("------------")
+print ("Poisson+LASSO MSE: ", mean_squared_error(labels_test, result_lcv))
 
 # Plot some informations
-plt.figure(1, figsize=(20, 10))
+plt.figure(figsize=(20, 10))
 plt.ylabel("Incidenza su 1000 persone")
 plt.xlabel("Settimane")
 plt.xticks(range(0, len(weeks)), weeks, rotation="vertical")
 
-plt.plot(range(0, len(result_lcv_imp)), result_lcv_imp, 'd-', label="LassoCV Model Standardized")
+plt.plot(range(0, len(result_lcv)), result_lcv, 'o-', label="Poisson Model")
 plt.plot(range(0, len(labels_test)), labels_test, 'x-', label="Actual Value")
 
 plt.legend()
 plt.margins(0.2)
 plt.subplots_adjust(bottom=0.15)
-plt.savefig(str(year_selected)+".png")
+plt.savefig(str(year_selected)+"_poisson.png")
 
 plt.figure(2, figsize=(20, 10))
 plt.ylabel("Numero di visite")
@@ -126,19 +138,17 @@ plt.xticks(range(0, len(weeks)), weeks, rotation="vertical")
 linestyles = ['-', '--', '-.', ':']
 colors =list(Color("blue").range_to(Color("red"), 10))
 for i in range(0, 10):
-    if i>=len(graph_pages):
-        break;
     plt.plot(range(0, len(data_graph[graph_pages[i][0]])), data_graph[graph_pages[i][0]], marker='.', color=colors[i].hex_l, label=graph_pages[i][0], linestyle=linestyles[i%len(linestyles)])
 
 lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., title="Pages Selected")
 plt.margins(0.2)
 plt.subplots_adjust(bottom=0.15)
-plt.savefig(str(year_selected)+"_feature.png", bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=200)
+plt.savefig(str(year_selected)+"_features_poisson.png", bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=200)
 
 # Write information to file
-with open(str(year_selected)+"_information.csv", 'w', newline='') as csvfile:
+with open(str(year_selected)+"_information_poisson.csv", 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['mse', mean_squared_error(labels_test, result_lcv_imp)])
+    spamwriter.writerow(['mse', mean_squared_error(labels_test, result_lcv)])
     for p in important_pages:
-        spamwriter.writerow([p[0], p[1]])
+        spamwriter.writerow([p[0], float(p[1])])
---
+++ models/models_utils.py
@@ -0,0 +1,139 @@
+import os
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+import glmnet_python
+from cvglmnet import cvglmnet
+from cvglmnetPredict import cvglmnetPredict
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+
+##### UTILITIES #######
+
+#  Feature path and labels
+path_features = "./../data/wikipedia_470";
+path_labels = "./../data/influnet/csv";
+
+def generate_features(year_a, year_b):
+    if not year_a.empty:
+        first_part= year_a.copy()[41:52]
+    else:
+        first_part = pd.DataFrame()
+    if not year_b.empty:
+        second_part= year_b.copy()[0:15]
+    else:
+        second_part = pd.DataFrame()
+
+    return first_part.append(second_part)
+
+# Generate feature list from the files. It takes also
+# a year which will be not included.
+def generate(stop_year):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_features)
+    file_list.sort()
+
+    for i in range(0, len(file_list)-1):
+        # If the file's year is equal than stop_year then do anything
+        if int(file_list[i].replace(".csv", "")) != stop_year-1 and int(file_list[i].replace(".csv", "")) != 2007:
+            tmp_a = pd.read_csv(os.path.join(path_features, file_list[i]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_a = pd.DataFrame()
+
+        if int(file_list[i+1].replace(".csv", "")) != stop_year:
+            tmp_b = pd.read_csv(os.path.join(path_features, file_list[i+1]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_b = pd.DataFrame()
+
+        # If the dataset is empty the generate a new dataframe.
+        # Append a new dataframe if the dataset is not empty.
+        if dataset.empty:
+            dataset = generate_features(tmp_a, tmp_b)
+        else:
+            dataset = dataset.append(generate_features(tmp_a, tmp_b))
+
+    return dataset
+
+def generate_one_year(year):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_features)
+    file_list.sort()
+
+    for i in range(0, len(file_list)-1):
+        if int(file_list[i].replace(".csv", "")) == year-1 and int(file_list[i].replace(".csv", "")) != 2007:
+            tmp_a = pd.read_csv(os.path.join(path_features, file_list[i]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_a = pd.DataFrame()
+
+        if int(file_list[i+1].replace(".csv", "")) == year:
+            tmp_b = pd.read_csv(os.path.join(path_features, file_list[i+1]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_b = pd.DataFrame()
+
+        # If the dataset is empty the generate a new dataframe.
+        # Append a new dataframe if the dataset is not empty.
+        if dataset.empty:
+            dataset = generate_features(tmp_a, tmp_b)
+        else:
+            dataset = dataset.append(generate_features(tmp_a, tmp_b))
+    return dataset
+
+def generate_labels(stop_year):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_labels)
+    file_list.sort()
+
+    for i in range(0, len(file_list)):
+        if (file_list[i] != "tabula-2006_2007.csv"):
+            # Read the file
+            _file = pd.read_csv(os.path.join(path_labels, file_list[i]))
+
+            # Append data without the stop year
+            years = file_list[i].replace("tabula-", "").split("_")
+            if int(years[1].replace(".csv", "")) != stop_year and int(years[0]) != 2007:
+                dataset = dataset.append(_file[0:11])
+            if int(years[0]) != stop_year-1:
+                dataset = dataset.append(_file[11:26])
+    return dataset
+
+def generate_labels_one_year(stop_year):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_labels)
+    file_list.sort()
+
+    for i in range(0, len(file_list)):
+        if (file_list[i] != "tabula-2006_2007.csv"):
+            # Read the file
+            _file = pd.read_csv(os.path.join(path_labels, file_list[i]))
+
+            # Append data without the stop year
+            years = file_list[i].replace("tabula-", "").split("_")
+            if int(years[1].replace(".csv", "")) == stop_year and int(years[0]) != 2007:
+                dataset = dataset.append(_file[0:11])
+            if int(years[0]) == stop_year-1:
+                dataset = dataset.append(_file[11:26])
+
+    return dataset
+
+def sort_cv(item):
+    return item[2]
+
+def cross_validation_glm(dataset, labels, data_imp, labels_test, runs=100):
+    values=[]
+    alphas = np.linspace(0.0, 1.0, num=runs)
+    for i in tqdm(range(runs)):
+        model = cvglmnet(x=dataset.copy(), y=labels.copy().as_matrix(), family='poisson', alpha=alphas[i], ptype="mse", parallel=True, nfolds=10)
+        result_model = cvglmnetPredict(model, data_imp, ptype = 'response', s = 'lambda_min')
+        score = mean_squared_error(labels_test, result_model)
+        values.append([alphas[i], model.copy(), score])
+    values = sorted(values, key=sort_cv)
+    return values
