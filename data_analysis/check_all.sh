feba1fea367898e6d8d8af1f2f6d9c853344ee11
Giovanni De Toni <giovanni.det@gmail.com>
2017-10-24 17:50:10 +0200

    [check_all] Update the script.

--- data_analysis/check_all.sh
+++ data_analysis/check_all.sh
@@ -14,21 +14,27 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Directory where all the Wikipedia log are located
-input_dir="/mnt/fluiddata/cconsonni/pagecounts-new/pagecounts/data/output"
+#input_dir="/mnt/fluiddata/cconsonni/pagecounts-new/pagecounts/data/output"
+input_dir="/mnt/fluiddata/cconsonni/pagecounts-new/pageviews/data/output/download/tmp/pagecounts/data/output"
+
+# Language of Wikipedia pages (like it, nl, de, etc.)
+language="de"
 
 # Directory where the generated files will be saved
-output_dir="./output/"
+output_dir="./output"
 
 # The keyword file which will be used to filter the Wikipedia logs
-keywords_file="./../data/keywords.txt"
+keywords_file="./../data/keywords/keywords_germany.txt"
 
-for year in {2009..2016}
+for year in {2015..2017}
 do
-    if [ $year == 2007]; then
-       all_files="$input_dir/$year-*"
+    if [ $year == 2007 ]; then
+       	#all_files="$input_dir/$year-*"
+       	all_files="$input_dir/output-$year*"
     else
-        all_files="$input_dir/$year-*/output-*"
+        #all_files="$input_dir/$year-*/output-*"
+    	all_files="$input_dir/output-$year*"
     fi
-    ./parse_dumps.sh -k "$keywords_file" -i "$all_files" -o "$output_dir$year" --debug
-    ./generate_weekly_data.py ".$output_dir/$year/result.output" "./output3/$year/$year.csv" -c "$keywords_file"
+    ./parse_dumps.sh -k "$keywords_file" -i "$all_files" -o "$output_dir/$year" -l "$language" --debug
+    ./generate_weekly_data.py "$output_dir/$year/result.output" "$output_dir/$year/$year.csv" -c "$keywords_file"
 done
