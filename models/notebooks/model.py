8be1a1b68161922a199890938c350645d8ec74ac
Giovanni De Toni <giovanni.det@gmail.com>
2017-10-27 11:52:41 +0200

    [data] Update dataset and add the germany+austria one.

---
+++ models/notebooks/model.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Machine learning model which uses Wikipedia data to predicts ILI levels.
+Usage:
+  model.py <year_start> <year_end> [--p]
+
+Options:
+  -p, --poisson Use the poisson model + LASSO
+  -h, --help
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from sklearn.linear_model import LassoCV, LassoLarsCV
+from sklearn.metrics import mean_squared_error
+from tabulate import tabulate
+from colour import Color
+from docopt import docopt
+import csv
+
+from models_utils import generate, generate_labels, generate_labels_one_year, generate_one_year, standardize_data, generate_keywords, stz, get_important_pages
+
+from cvglmnetCoef import cvglmnetCoef
+from cvglmnetPredict import cvglmnetPredict
+from cvglmnet import cvglmnet
+
+#### INITIALIZATION #####
+
+# Parse the command line
+arguments = docopt(__doc__)
+
+# Year selected
+year_start = int(arguments["<year_start>"])
+year_end = int(arguments["<year_end>"])+1
+
+# Model type
+using_poisson = False
+if (arguments["--p"]):
+    using_poisson = True
+
+# Feature path and labels
+path_features = "./../data/wikipedia_austria/old_data"
+path_labels = "./../data/austria/old_data"
+keywords = "../data/keywords/keywords_germany.txt"
+
+# Selected columns that will be extracted from the dataframes
+selected_columns = generate_keywords(keywords)
+
+##### ALGORITHM ######
+
+all_features_values = pd.DataFrame()
+all_predicted_values = []
+all_true_labels = pd.DataFrame()
+total_weeks = []
+all_weighted_feature=dict()
+
+year_sel = (year_start, year_end)
+
+for year_selected in range(year_sel[0], year_sel[1]):
+
+    #print("[*] ", year_selected )
+    # Data generation from data files
+    dataset = generate(year_selected, path_features)[selected_columns]
+    labels = generate_labels(year_selected, path_labels)
+    data = generate_one_year(year_selected, path_features)[selected_columns]
+    labels_test = generate_labels_one_year(year_selected, path_labels)
+    weeks = labels_test["week"]
+
+    # Create a copy of the dataset where we fills NaN values
+    # with 0.
+    dataset_zero = dataset.fillna(0)
+    data_zero = data.fillna(0)
+
+    # Standardize data
+    train, test = standardize_data(dataset_zero, data_zero)
+
+    # Regenerate panda dataframe with standardized data
+    data_graph = pd.DataFrame(data=test, columns=selected_columns)
+
+    # Create a Lasso Cross-Validation instance which will be
+    # trained on the dataset in which NaN values are replaced
+    # with the column mean.
+    if not using_poisson:
+        model = LassoCV(max_iter=100000, n_jobs=-1, n_alphas=1000, random_state=1)
+        model.fit(train, labels["incidence"].fillna(0))
+        result = model.predict(test)
+    else:
+        model = cvglmnet(x=train.as_matrix().copy(), y=labels["incidence"].fillna(0).copy().as_matrix(), family='poisson', alpha=1.0,
+                       ptype="mse", parallel=True, nfolds=10)
+        result = cvglmnetPredict(model, test.as_matrix(), ptype='response', s="lambda_min")
+        coeff = cvglmnetCoef(model, s="lambda_min")
+
+    # Get the feature coefficients
+    if not using_poisson:
+        coeff = model.coef_
+    else:
+        coeff = cvglmnetCoef(model, s="lambda_min")
+
+    # Add the pair (value, coeff) to a dictionary
+    for i in list(zip(coeff, selected_columns)):
+        if (i[0] != 0):
+            if all_weighted_feature.get(i[1], None) is None:
+                all_weighted_feature[i[1]] = [i[0]]
+            else:
+                all_weighted_feature[i[1]].append(i[0])
+
+    # Add the value to global variables
+    all_features_values= all_features_values.append(data.fillna(0))
+    all_predicted_values.extend(result)
+    all_true_labels = all_true_labels.append(labels_test)
+    total_weeks.extend(weeks)
+
+
+# Get important pages to generate the plot we need
+important_pages = get_important_pages(all_weighted_feature, 5)
+
+# Evaluate the model
+mse = mean_squared_error(all_true_labels["incidence"].fillna(0), all_predicted_values)
+pcc = np.corrcoef(all_predicted_values, all_true_labels["incidence"].fillna(0), rowvar=False)[0][1]
+
+# Print the evaluation result
+print(year_start, "-", year_end-1, ",", mse, ",", pcc)
+
+# Plot some informations
+fig = plt.figure(3, figsize=(15, 6))
+ax = fig.add_subplot(111)
+
+plt.title("Stagioni Influenzali "+str(year_sel[0]-1)+" - "+str(year_sel[1]-1), fontsize=18)
+plt.ylabel("Incidenza su 100000 persone", fontsize=17)
+plt.xlabel("Anno-Settimana", fontsize=17)
+
+weeks_used = []
+for k, v in enumerate(total_weeks):
+    if k%2==0:
+        weeks_used.append(v)
+    else:
+        weeks_used.append(" ")
+
+plt.xticks(range(0, len(weeks_used)), weeks_used, rotation="vertical", fontsize=15)
+plt.yticks(fontsize=15)
+
+plt.plot(range(0, len(all_predicted_values)), all_predicted_values, 'r-', label="Modello Lineare", linewidth=3)
+plt.plot(range(0, len(all_true_labels["incidence"])), all_true_labels["incidence"].fillna(0), 'k-', label="InfluNet", linewidth=3)
+
+# Add dotted line into the graph to delimit the influenza seasons.
+for i in range(1, (year_sel[1]-year_sel[0])):
+    plt.axvline(26*i, linestyle="dashed", color="k", linewidth=3)
+
+# Update some graphical options
+plt.grid()
+plt.legend(fontsize=16, loc="upper right")
+plt.tight_layout()
+
+# Save the grapg on file
+plt.savefig("appendix_linear_"+str(year_sel[0]-1)+"-"+str(year_sel[1]-1)+".png", dpi=150)
+
+# Feature plot
+figf = plt.figure(4, figsize=(15, 6))
+axf = fig.add_subplot(111)
+
+plt.title("Variazione pageview "+str(year_sel[0]-1)+" - "+str(year_sel[1]-1), fontsize=18)
+plt.ylabel("Numero Pageview", fontsize=17)
+plt.xlabel("Anno-Settimana", fontsize=17)
+
+plt.xticks(range(0, len(weeks_used)), weeks_used, rotation="vertical", fontsize=15)
+plt.yticks(fontsize=15)
+
+std_all_features_values = stz(all_features_values)
+for key, value in important_pages:
+ plt.plot(range(0, len(std_all_features_values[key])), std_all_features_values[key], '-', label=key, linewidth=3)
+plt.plot(range(0, len(all_true_labels["incidence"])), stz(all_true_labels["incidence"]), 'k-', label="InfluNet", linewidth=3)
+
+plt.grid()
+plt.legend(fontsize=16, loc="best")
+plt.tight_layout()
+
+plt.savefig("appendix_linear_feature_"+str(year_sel[0]-1)+"-"+str(year_sel[1]-1)+".png", dpi=150)
---
+++ models/notebooks/models_utils.py
@@ -0,0 +1,198 @@
+import os
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+import glmnet_python
+from cvglmnet import cvglmnet
+from cvglmnetPredict import cvglmnetPredict
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+
+##### UTILITIES #######
+
+def generate_keywords(keywords = "../data/keywords/keywords_italy.txt"):
+    selected_columns = []
+    file_ = open(keywords, "r")
+    for line in file_:
+        if line != "Week":
+            selected_columns.append(line.replace("\n", "").replace("\\", ""))
+    return selected_columns
+
+def generate_features(year_a, year_b, number_a, number_b):
+    if not year_a.empty:
+        if (number_a != 2007):
+            first_part= year_a.copy()[41:52]
+        else:
+            first_part= year_a.copy()[48:52]
+    else:
+        first_part = pd.DataFrame()
+    if not year_b.empty and number_b != 2007:
+        second_part= year_b.copy()[0:15]
+    else:
+        second_part = pd.DataFrame()
+
+    return first_part.append(second_part)
+
+# Generate feature list from the files. It takes also
+# a year which will be not included.
+def generate(stop_year, path_features="./../data/wikipedia_italy/new_data"):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_features)
+    file_list.sort()
+
+    for i in range(0, len(file_list)-1):
+        # If the file's year is equal than stop_year then do anything
+        if int(file_list[i].replace(".csv", "")) != stop_year-1:
+            tmp_a = pd.read_csv(os.path.join(path_features, file_list[i]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_a = pd.DataFrame()
+
+        if int(file_list[i+1].replace(".csv", "")) != stop_year:
+            tmp_b = pd.read_csv(os.path.join(path_features, file_list[i+1]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_b = pd.DataFrame()
+
+        # If the dataset is empty the generate a new dataframe.
+        # Append a new dataframe if the dataset is not empty.
+        if dataset.empty:
+            dataset = generate_features(tmp_a, tmp_b, int(file_list[i].replace(".csv", "")), int(file_list[i+1].replace(".csv", "")))
+        else:
+            dataset = dataset.append(generate_features(tmp_a, tmp_b, int(file_list[i].replace(".csv", "")), int(file_list[i+1].replace(".csv", ""))))
+
+    return dataset
+
+def generate_one_year(year, path_features="./../data/wikipedia_italy/new_data"):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_features)
+    file_list.sort()
+
+    for i in range(0, len(file_list)-1):
+        if int(file_list[i].replace(".csv", "")) == year-1:
+            tmp_a = pd.read_csv(os.path.join(path_features, file_list[i]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_a = pd.DataFrame()
+
+        if int(file_list[i+1].replace(".csv", "")) == year:
+            tmp_b = pd.read_csv(os.path.join(path_features, file_list[i+1]), encoding = 'utf8', delimiter=',')
+        else:
+            tmp_b = pd.DataFrame()
+
+        # If the dataset is empty the generate a new dataframe.
+        # Append a new dataframe if the dataset is not empty.
+        if dataset.empty:
+            dataset = generate_features(tmp_a, tmp_b, int(file_list[i].replace(".csv", "")), int(file_list[i+1].replace(".csv", "")))
+        else:
+            dataset = dataset.append(generate_features(tmp_a, tmp_b, int(file_list[i].replace(".csv", "")), int(file_list[i+1].replace(".csv", ""))))
+    return dataset
+
+def generate_labels(stop_year, path_labels="./../data/italy/new_data"):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_labels)
+    file_list.sort()
+
+    for i in range(0, len(file_list)):
+        if (file_list[i] != "tabula-2006_2007.csv"):
+            # Read the file
+            _file = pd.read_csv(os.path.join(path_labels, file_list[i]))
+
+            # Append data without the stop year
+            years = file_list[i].split("_")
+            if int(years[1].replace(".csv", "")) != stop_year:
+                if int(years[0]) == 2007:
+                    dataset = dataset.append(_file[7:11])
+                else:
+                    dataset = dataset.append(_file[0:11])
+            if int(years[0]) != stop_year-1:
+                dataset = dataset.append(_file[11:26])
+    return dataset
+
+def generate_labels_one_year(stop_year, path_labels="./../data/italy/new_data"):
+    dataset = pd.DataFrame()
+
+    # Get all features files and sort the list
+    file_list = os.listdir(path_labels)
+    file_list.sort()
+
+    for i in range(0, len(file_list)):
+        if (file_list[i] != "tabula-2006_2007.csv"):
+            # Read the file
+            _file = pd.read_csv(os.path.join(path_labels, file_list[i]))
+
+            # Append data without the stop year
+            years = file_list[i].replace("tabula-", "").split("_")
+            if int(years[1].replace(".csv", "")) == stop_year:
+                if int(years[0]) == 2007:
+                    dataset = dataset.append(_file[7:11])
+                else:
+                    dataset = dataset.append(_file[0:11])
+            if int(years[0]) == stop_year-1:
+                dataset = dataset.append(_file[11:26])
+
+    return dataset
+
+def generate_labels_sum():
+    # Get all features files and sort the list
+    file_list = os.listdir("./../data/austria/old_data")
+    file_list.sort()
+    for i in range(0, len(file_list)):
+        _file = pd.read_csv(os.path.join("./../data/austria/old_data", file_list[i]))
+        _file_2 = pd.read_csv(os.path.join("./../data/germany/old_data", file_list[i]))
+        total = pd.DataFrame()
+        total['week'] = _file['week']
+        total['incidence'] = stz(stz(_file['incidence']) + stz(_file_2['incidence']))
+        total.to_csv(file_list[i])
+
+def standardize_data(train, test):
+    data_total = np.concatenate((train, test), axis=0)
+    dmean = data_total.mean(axis=0)
+    dmax = data_total.max(axis=0)
+    dmin = data_total.min(axis=0)
+    dmax_min = dmax - dmin
+    dataset_imp = (train - dmean) / dmax_min
+    data_imp = (test - dmean) / dmax_min
+    dataset_imp[np.isnan(dataset_imp)] = 0
+    data_imp[np.isnan(data_imp)] = 0
+    return (dataset_imp, data_imp)
+
+def stz(data):
+    dmean = data.mean(axis=0)
+    dmax = data.max(axis=0)
+    dmin = data.min(axis=0)
+    dmax_min = dmax - dmin
+    dataset_imp = (data - dmean) / dmax_min
+    dataset_imp[np.isnan(dataset_imp)] = 0
+    return dataset_imp
+
+def sort_cv(item):
+    return item[2]
+
+def cross_validation_glm(dataset, labels, data_imp, labels_test, runs=100):
+    values=[]
+    alphas = np.linspace(0.0, 1.0, num=runs)
+    for i in tqdm(range(runs)):
+        model = cvglmnet(x=dataset.copy(), y=labels.copy().as_matrix(), family='poisson', alpha=alphas[i], ptype="mse", parallel=True, nfolds=10)
+        result_model = cvglmnetPredict(model, data_imp, ptype = 'response', s = 'lambda_min')
+        score = mean_squared_error(labels_test, result_model)
+        values.append([alphas[i], model.copy(), score])
+    values = sorted(values, key=sort_cv)
+    return values
+
+
+def getKey(item):
+    return item[1]
+
+def get_important_pages(important_pages, top=10):
+    imp_pages_avg = dict((k, sum(v) / float(len(v))) for k, v in important_pages.items())
+    _terms_avg_top = sorted(sorted(imp_pages_avg.items(),
+                                   key=lambda value: value[0]),
+                            key=lambda value: value[1],
+                            reverse=True
+                            )
+    return _terms_avg_top[0:top]
