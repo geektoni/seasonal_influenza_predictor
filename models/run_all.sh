6e1f36d5ccc7fea93055e508c2a4145e3e015391
Giovanni De Toni <giovanni.det@gmail.com>
2018-06-20 10:49:50 +0200

    [data/run_all.sh] Add country to the model.py command.

--- models/run_all.sh
+++ models/run_all.sh
@@ -26,8 +26,8 @@ do
 		start_year=$(cut -d'-' -f1 <<< $line)
 		end_year=$(cut -d'-' -f2 <<< $line)
 		directory=$base_dir/$c/
-        command="./model.py $start_year $end_year ./../data/wikipedia_${c}/$type_of_data ./../data/$c/$type_of_data ./../data/keywords/keywords_${c}.txt --f --d $directory --no-images"
-        command2="./model.py $((start_year+1)) $end_year ./../data/wikipedia_${c}/$type_of_data ./../data/$c/$type_of_data ./../data/keywords/keywords_${c}.txt --f --d $directory --no-images -no--future"
+        command="./model.py $start_year $end_year ./../data/wikipedia_${c}/$type_of_data ./../data/$c/$type_of_data ./../data/keywords/keywords_${c}.txt $c --f --d $directory --no-images"
+        command2="./model.py $((start_year+1)) $end_year ./../data/wikipedia_${c}/$type_of_data ./../data/$c/$type_of_data ./../data/keywords/keywords_${c}.txt $c --f --d $directory --no-images -no--future"
         echo $command
         eval $command
         echo $command2
