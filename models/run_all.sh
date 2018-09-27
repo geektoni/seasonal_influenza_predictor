0aa92949d83219ec2b8d8ba783fd1520b1c23968
Giovanni De Toni <giovanni.det@gmail.com>
2018-09-27 15:09:46 +0200

    [model] Fix run_all.sh script.

--- models/run_all.sh
+++ models/run_all.sh
@@ -9,7 +9,7 @@ country_list=("germany" "austria" "italy" "netherlands")
 year_list=("2008-2016" "2013-2016" "2008-2016" "2010-2016")
 year_list_future=("2008-2018" "2013-2018" "2008-2018" "2010-2018")
 type_of_exec=("future" "no-future")
-type_of_data=("old_data" "new_data")
+type_of_data=("old_data" "new_data" "pageviews")
 base_dir="./complete_results"
 
 for data_type in ${type_of_data[@]}
@@ -26,8 +26,17 @@ do
         	    mkdir -p $base_dir/$data_type/$type_exec/$c
     	    fi
 
-    	    start_year="$(echo ${year_list[$counter]} | cut -f 1 -d -)"
-    	    end_year="$(echo ${year_list[$counter]} | cut -f 2 -d -)"
+            if [ $data_type == "old_data" ]; then
+    	        start_year="$(echo ${year_list[$counter]} | cut -f 1 -d -)"
+                end_year="$(echo ${year_list[$counter]} | cut -f 2 -d -)"
+            elif [ $data_type == "new_data" ]; then
+                start_year="$(echo ${year_list_future[$counter]} | cut -f 1 -d -)"
+                end_year="$(echo ${year_list_future[$counter]} | cut -f 2 -d -)"
+            else
+                start_year="2016"
+                end_year="2018"
+            fi
+
 
             # Check which type of execution we need to perform.
             if [ $type_exec == "no-future" ]; then
