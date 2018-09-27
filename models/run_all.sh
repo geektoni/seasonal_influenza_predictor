f7f4914f8466d371ccb179e383283521036cfbb8
Giovanni De Toni <giovanni.det@gmail.com>
2018-09-27 10:52:57 +0200

    [model] Update run_all.sh script to generate correctly everything needed.

--- models/run_all.sh
+++ models/run_all.sh
@@ -6,41 +6,42 @@ set -euo pipefail
 IFS=$'\n\t'
 
 country_list=("germany" "austria" "italy" "netherlands")
-year_lists=("./year_lists_old" "./year_lists_new")
-type_of_data=""
-base_dir=""
+year_list=("2008-2016" "2013-2016" "2008-2016" "2010-2016")
+year_list_future=("2008-2018" "2013-2018" "2008-2018" "2010-2018")
+type_of_exec=("future" "no-future")
+type_of_data=("old_data" "new_data")
+base_dir="./complete_results"
 
-for year_list in ${year_lists[@]}
+for data_type in ${type_of_data[@]}
 do
-	if [ "$year_list" = "./year_lists_old" ]; then
-		type_of_data="old_data"
-		base_dir="files_old"
-	else
-		type_of_data="new_data"
-		base_dir="files"
-	fi
+    for type_exec in ${type_of_exec[@]}
+    do
+        counter=0;
+	    for c in ${country_list[@]}
+	    do
 
-	# Generate all directories
-	for c in ${country_list[@]}
-	do
-    	if [ ! -d $base_dir/$c ]; then
-        	mkdir -p $base_dir/$c
-    	fi
-	done
+            # Create the directory, if it doesn't exists
+	        directory=$base_dir/$data_type/$type_exec/$c
+    	    if [ ! -d $base_dir/$c ]; then
+        	    mkdir -p $base_dir/$data_type/$type_exec/$c
+    	    fi
 
-	for c in ${country_list[@]}
-	do
-    	for line in `cat $year_list/$c.txt`
-    	do
-			start_year=$(cut -d'-' -f1 <<< $line)
-			end_year=$(cut -d'-' -f2 <<< $line)
-			directory=$base_dir/$c/
-    	    commanda="./model.py $start_year $end_year ./../data/wikipedia_${c}/$type_of_data ./../data/$c/$type_of_data ./../data/keywords/keywords_${c}.txt $c --f --d $directory --no-images"
-    	    commandb="./model.py $((start_year+1)) $end_year ./../data/wikipedia_${c}/$type_of_data ./../data/$c/$type_of_data ./../data/keywords/keywords_${c}.txt $c --f --d $directory --no-future --no-images"
-    	    echo $commanda
-    	    eval $commanda
-			echo $commandb
-			eval $commandb
-    	done
-	done
+    	    start_year="$(echo ${year_list[$counter]} | cut -f 1 -d -)"
+    	    end_year="$(echo ${year_list[$counter]} | cut -f 2 -d -)"
+
+            # Check which type of execution we need to perform.
+            if [ $type_exec == "no-future" ]; then
+                command="./model.py $start_year $end_year ./../data/wikipedia_${c}/$data_type ./../data/$c/$data_type ./../data/keywords/keywords_${c}.txt $c --f --d $directory --no-future"
+            else
+                command="./model.py $start_year $end_year ./../data/wikipedia_${c}/$data_type ./../data/$c/$data_type ./../data/keywords/keywords_${c}.txt $c --f --d $directory"
+            fi
+
+            # Evaluate the command
+            echo $command
+            eval $command
+
+            counter=$counter+1
+	    done
+
+    done
 done
