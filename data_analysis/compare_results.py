3649ca899b1807575d1b3986950e4f951fbf108e
Giovanni De Toni <giovanni.det@gmail.com>
2018-12-02 22:57:11 +0100

    [data_analysis] Improved compare_results by using MSE.

--- data_analysis/compare_results.py
+++ data_analysis/compare_results.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-Usage: compare_results.py <result_a> <result_b> [--f]
+Usage: compare_results.py <result_a> <result_b>
 """
 
 from docopt import docopt
 
 import pandas as pd
+from sklearn.metrics import mean_squared_error
 
 if __name__ == "__main__":
 
@@ -18,8 +19,12 @@ if __name__ == "__main__":
 	rb = pd.read_csv(args["<result_b>"])
 
 	results = pd.merge(ra, rb, on="week")
-	results["diff_x"] = pow(results["incidence_x"]-results["prediction_x"], 2)
-	results["diff_y"] = pow(results["incidence_y"]-results["prediction_y"], 2)
-	results["improvement_rate"] = results["diff_x"]-results["diff_y"]
+	mse_a = mean_squared_error(results["incidence_x"], results["prediction_x"])
+	mse_b = mean_squared_error(results["incidence_y"], results["prediction_y"])
 
-	print(results["improvement_rate"])
+	improvement_rate = mse_a - mse_b
+
+
+	print("MSE file A: {}".format(mse_a))
+	print("MSE file B: {}".format(mse_b))
+	print("Improvement Rate A-B: {}".format(improvement_rate))
