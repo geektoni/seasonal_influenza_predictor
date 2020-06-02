335faaa57931502d672f634c6e0e5faf182773d1
Giovanni De Toni <giovanni.det@gmail.com>
2020-06-02 10:52:17 +0200

    Improve better_compare_features.py

--- data_analysis/better_compare_features.py
+++ data_analysis/better_compare_features.py
@@ -22,7 +22,7 @@ def get_keywords_prefix(model):
     """
     if model == "cyclerank" or model == "cyclerank_pageviews":
         return "keywords_cyclerank"
-    elif model == "pagerank" or model == "pagerank":
+    elif model == "pagerank" or model == "pagerank_pageviews":
         return "keywords_pagerank"
     else:
         return "keywords"
@@ -52,7 +52,7 @@ def get_intersection_dataframe(original_data):
         for second in original_data:
             intersection_value = set.intersection(set(original_data[first].page_name),
                                                   set(original_data[second].page_name))
-            partial_value.append(len(intersection_value))
+            partial_value.append(len(intersection_value)/len(original_data[first])*100)
         intersection_df.append(partial_value)
 
     # Convert the intersection into a dataframe
@@ -61,7 +61,36 @@ def get_intersection_dataframe(original_data):
     intersection_df = intersection_df.set_index("index")
     return intersection_df
 
+
+def get_min_max_mean_feature_selected(df, start_year=2015, end_year=2019):
+    min = 100000
+    max = 0
+    mean = 0
+    seasons = df.season.unique()
+    for s in seasons:
+        start_end = s.split("-")
+        if int(start_end[0]) >= start_year and int(start_end[1]) <= end_year:
+            features_slice = df[df.season == s]
+            features_selected = len(features_slice)
+
+            if features_selected > max:
+                max = features_selected
+            if features_selected < min:
+                min = features_selected
+            mean += features_selected
+
+    return min, max, mean/len(seasons)
+
 def get_feature_dictionary(df, start_year=2015, end_year=2019):
+    """
+    Group all the features into a dictionary. Namely, we generate for each
+    feature a list of values which indicates their weight in each of the models
+    under scrutiny
+    :param df: the feature dataframe
+    :param start_year: start year to consider
+    :param end_year: end year to consider
+    :return: a dictionary
+    """
 
     feature_dictionary = {}
     seasons = df.season.unique()
@@ -77,16 +106,16 @@ def get_feature_dictionary(df, start_year=2015, end_year=2019):
     return feature_dictionary
 
 
-def get_important_pages(important_pages, top=10):
+def get_important_pages(important_pages, top=10, influenza_seasons=4):
     """
     Get the most important feature selected by the model.
 
     :param important_pages: a dictionary with, for each of the features,
     a list of their weights in each of the models.
     :param top: how many feature we want to return.
-    :return: the top feauture
+    :return: the top feature
     """
-    imp_pages_avg = dict((k, sum(v) / float(len(v))) for k, v in important_pages.items())
+    imp_pages_avg = dict((k, sum(v) / float(influenza_seasons)) for k, v in important_pages.items())
     _terms_avg_top = sorted(sorted(imp_pages_avg.items(),
                                    key=lambda value: value[0]),
                             key=lambda value: value[1],
@@ -106,6 +135,7 @@ if __name__ == "__main__":
     parser.add_argument("--future", default=False, action="store_true")
     parser.add_argument("--start-year", default=2015, type=int)
     parser.add_argument("--end-year", default=2019, type=int)
+    parser.add_argument("--save", default=False, action="store_true")
     args = parser.parse_args()
 
     # Set some variables
@@ -133,21 +163,43 @@ if __name__ == "__main__":
 
         # Compute the best features over all models
         feature_dictionary = get_feature_dictionary(features)
-        best_features = get_important_pages(feature_dictionary, top=20)
+        best_features = get_important_pages(feature_dictionary, top=300)
         best_features = [[x[0], x[1]] for x in best_features]
 
         total_features[model_name] = pd.DataFrame(best_features, columns=["page_name", "mean_weigth"])
 
+        # Get min, max and mean features
+        min, max, mean = get_min_max_mean_feature_selected(features)
+        print("\t{}\t{}\t{}\t{:.2f}".format(convert_naming(model_name), min, max, mean))
 
-    #sns.heatmap(get_intersection_dataframe(total_keywords), annot=True, fmt="d")
+
+
+    sns.heatmap(get_intersection_dataframe(total_keywords), annot=True, fmt="f")
+    plt.tight_layout()
+    plt.show()
+
+    # Find the common best feature between the various models
     intersection_features = get_intersection_dataframe(total_features)
-    sns.heatmap(intersection_features, annot=True, fmt="d",
+    sns.heatmap(intersection_features, annot=True, fmt="f",
                 cmap=sns.cubehelix_palette(8, start=.5, rot=-.75), linewidths=.5)
 
-    plt.tight_layout()
+    # Save the top-5 feature for each model
+    top_5_features = pd.DataFrame(columns=list(total_features.keys()))
+    for k in total_features:
+        top_5_features[k] = total_features[k]["page_name"][0:5].apply(lambda x: x.replace("_", " "))
+
+
     plt.ylabel("")
     plt.title(country.title())
-    plt.show()
-    #plt.savefig(save_filename, dpi=200, bbox_inches='tight')
+    plt.tight_layout()
+
+    if not args.save:
+        plt.show()
+    else:
+        save_filename = "common_feature_{}.png".format(country)
+        plt.savefig(save_filename, dpi=200, bbox_inches='tight')
+
+        save_filename = "top_5_features_{}.csv".format(country)
+        top_5_features.to_csv(save_filename, index=False)
 
 
